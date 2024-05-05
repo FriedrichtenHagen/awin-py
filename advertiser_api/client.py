f834584691ee9d17855f3563aebc130b211f2963
Friedrich ten Hagen <friedrich.ten.hagen@googlemail.com>
2024-05-05 12:26:40 +0200

    Add type hints and docstring

--- advertiser_api/client.py
+++ advertiser_api/client.py
@@ -4,9 +4,10 @@ import requests
 from dotenv import load_dotenv
 from pprint import pprint
 from datetime import datetime, timedelta
+import time
+from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union
 
-
-from advertiser_api.errors import AwinError, PersonioApiError
+from advertiser_api.errors import AwinError, AwinApiError
 """
 Implementation of the Awin API functions
 
@@ -16,7 +17,7 @@ Docs: https://wiki.awin.com/index.php/Advertiser_API
 class Awin:
 
     BASE_URL = "https://api.awin.com/"
-    """base URL of the Personio HTTP API"""
+    """base URL of the Awin HTTP API"""
 
     def __init__(self, base_url=None, client_id=None, client_secret=None):
         self.base_url = base_url or self.BASE_URL
@@ -29,15 +30,15 @@ class Awin:
             "Authorization": f"Bearer {self.client_secret}"
         }
 
-    def request(self, path, params=None, method='GET'):
+    def _request(self, path, params=None, method='GET') -> List[Dict[str, Any]]:
             """
             Make a request against the AWIN API.
             Returns the HTTP response, which might be successful or not.
 
-            :param path: the URL path for this request (relative to the Personio API base URL)
-            :param method: the HTTP request method (default: GET)
+            :param path: the URL path for this request (relative to the Awin API base URL)
             :param params: dictionary of URL parameters (optional)
-            :param headers: contains the api secret
+            :param method: the HTTP request method (default: GET)
+            :return: the parsed json response, when the request was successful, or a AwinApiError
             """
             # make the request
             url = urljoin(self.base_url, path)
@@ -48,9 +49,9 @@ class Awin:
                 except ValueError:
                     raise AwinError(f"Failed to parse response as json: {response.text}")
             else:
-                raise PersonioApiError.from_response(response)
+                raise AwinApiError.from_response(response)
     
-    def get_accounts(self):
+    def get_accounts(self) -> List[Dict[str, Any]]:
         """
         GET accounts
         provides a list of accounts you have access to
@@ -59,10 +60,10 @@ class Awin:
 
         https://wiki.awin.com/index.php/API_get_accounts
         """
-        accounts = self.request('accounts')
+        accounts = self._request('accounts')
         return accounts
         
-    def get_publishers(self):
+    def get_publishers(self) -> List[Dict[str, Any]]:
         """
         GET publishers
         provides a list of publishers you have an active relationship with
@@ -71,14 +72,34 @@ class Awin:
 
         https://wiki.awin.com/index.php/API_get_publishers
         """
-        publishers = self.request(f'advertisers/{self.client_id}/publishers')
+        publishers = self._request(f'advertisers/{self.client_id}/publishers')
         return publishers
 
-    def get_transactions(self, start_date, end_date, date_type='transaction', timezone='UTC', status=None, publisher_id=None, show_basket_products=None):
+    def get_transactions(self, start_date, end_date, date_type='transaction', timezone='UTC', status=None, publisher_id=None, show_basket_products=None)  -> List[Dict[str, Any]]:
         """
         GET transactions (list)
         provides a list of your individual transactions
 
+        :param start_date: date object that specifies the beginning of the selected date range
+        :param end_date: date object that specifies the end of the selected date range
+        :param date_type: The type of date by which the transactions are selected. Can be 'transaction' or 'validation'. (optional)
+        :param timezone: Can be one of the following:
+            Europe/Berlin
+            Europe/Paris
+            Europe/London
+            Europe/Dublin
+            Canada/Eastern
+            Canada/Central
+            Canada/Mountain
+            Canada/Pacific
+            US/Eastern
+            US/Central
+            US/Mountain
+            US/Pacific
+            UTC
+        :param status: Filter by transaction status. Can be one of the following: pending, approved, declined, deleted
+        :param publisherId: Allows filtering by publisher id. Example: 12345 or 12345,67890 for multiple ones
+        :param show_basket_products: If &showBasketProducts=true then products sent via Product Level Tracking matched to the transaction can be viewed
         :return: list of ``transaction`` instances
 
         https://wiki.awin.com/index.php/API_get_transactions_list
@@ -91,7 +112,7 @@ class Awin:
             number_of_requests += 1
         print(f'number of request: {number_of_requests}')
 
-        # paginage in steps of 31 days
+        # paginate in steps of 31 days
         result = []
         for i in range(number_of_requests):
             print(f'request number {i}')
@@ -126,15 +147,15 @@ class Awin:
                 'publisherId': publisher_id,
                 'showBasketProducts': show_basket_products	
             }
-            transactions = self.request(f'advertisers/{self.client_id}/transactions/', params)
+
+            # make sure rate limit is not reached
+            if i % 20 == 0 and i > 0:
+                time.sleep(60)
+
+            transactions = self._request(f'advertisers/{self.client_id}/transactions/', params)
             result.append(transactions)
-
         return result
-
-
-
-
-
+    
 
     # GET transactions (by ID)
     # provides individual transactions by ID
